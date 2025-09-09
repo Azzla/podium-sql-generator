c71ace5d116caca57d22a67dfd38043aeb3fb41f
Carson <azzlamusic@hotmail.com>
2025-09-09 14:12:35 -0600

    fixed distinct tasks query bug

--- main.js
+++ main.js
@@ -3,21 +3,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
     var results_area    = document.getElementById("result");
 
     submit_button.addEventListener("click", function(e) {
-        //Get all elements
         const integration = document.getElementById("integration");
         const loc_uid     = document.getElementById("location");
         const task_type   = document.getElementById("task_type");
         const max_items   = document.getElementById("limit");
         const breadth     = document.querySelector("input[name='breadth']:checked");
         const order       = document.querySelector("input[name='order']:checked");
-
-        // Column 2
         const date_from   = document.getElementById("date-from");
         const date_to     = document.getElementById("date-to");
         const payload_str = document.getElementById("payload-string");
 
-        //Build individual query components
-        const breadth_string     = `select ${breadth.value} from BUILD.MARKETPLACE.BASE_EXECUTIONS_BRIDGE `;
+        //insert this extra string after 'distinct' and before 'from'
+        var breadth_p1 = `select ${breadth.value} `
+        if (breadth.value === 'distinct') {
+            breadth_p1 += 'bridge_execution_completed_tasks[0] ';
+        }
+
+        //build individual query elements
+        const breadth_string     = breadth_p1 + 'from BUILD.MARKETPLACE.BASE_EXECUTIONS_BRIDGE ';
         const integration_string = `where bridge_execution_integration_name = '${integration.value}' `;
         const loc_uid_string     = `and location_uid = '${loc_uid.value}' `;
         const task_type_string   = `and bridge_execution_completed_tasks[0] = '${task_type.value}' `;
@@ -27,7 +30,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         const limit_string       = `limit ${max_items.value};`;
 
         //Construct final query string
-        results_area.value = breadth_string + integration_string + loc_uid_string         
+        results_area.value = breadth_string + integration_string + loc_uid_string;
 
         if (task_type.value !== "") {
             results_area.value = results_area.value + task_type_string;
